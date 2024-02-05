bda893e96f99cd81485f49e066a007775467d9df
anessiinformatica <97632755+anessiinformatica@users.noreply.github.com>
2024-02-04 22:46:58 -0300

    Add files via upload

---
+++ crearvm.ps1
@@ -0,0 +1,18 @@
+﻿#Genero grupo de recursos
+New-AzResourceGroup -Name 'myResourceGroup' -Location 'EastUS'
+
+#Genero configuracion basica de VM
+New-AzVm `
+    -ResourceGroupName 'myResourceGroup' `
+    -Name 'mymaquina' `
+    -Location 'EastUS' `
+    -Image 'MicrosoftWindowsServer:WindowsServer:2022-datacenter-azure-edition:latest' `
+    -VirtualNetworkName 'myVnet' `
+    -SubnetName 'mySubnet' `
+    -SecurityGroupName 'myNetworkSecurityGroup' `
+    -PublicIpAddressName 'myPublicIpAddress' `
+    -OpenPorts 80,3389
+
+
+#Ejecuto script para IIS Microsoft
+Invoke-AzVMRunCommand -ResourceGroupName 'myResourceGroup' -VMName 'mymaquina' -CommandId 'RunPowerShellScript' -ScriptString 'Install-WindowsFeature -Name Web-Server -IncludeManagementTools'
